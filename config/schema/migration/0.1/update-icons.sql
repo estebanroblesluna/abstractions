7d9241a61f690b914dab7edce78565e7a5d2222a
matias <jose.matias.rivero@gmail.com>
2013-10-08 16:53:40 -0300

    Mege

--- config/schema/migration/0.1/update-icons.sql
+++ config/schema/migration/0.1/update-icons.sql
@@ -19,3 +19,23 @@ WHERE element_definition_id = 10;
 UPDATE `element_definition` 
 SET `icon` = 'Resources/wiretap.gif'
 WHERE element_definition_id = 7;
+
+UPDATE `element_definition` 
+SET `icon` = 'Resources/httpFetcher.gif'
+WHERE element_definition_id = 9;
+
+UPDATE `element_definition` 
+SET `icon` = 'Resources/fileReader.gif'
+WHERE element_definition_id = 11;
+
+UPDATE `element_definition` 
+SET `icon` = 'Resources/log.png'
+WHERE element_definition_id = 13;
+
+UPDATE `element_definition` 
+SET `icon` = 'Resources/chainConnection.gif'
+WHERE element_definition_id = 18;
+
+UPDATE `element_definition` 
+SET `icon` = 'Resources/null.png'
+WHERE element_definition_id = 19;
