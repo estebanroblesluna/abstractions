3f4b5d0e34aa1c5870bb61d74197c325768b994d
Esteban Robles Luna <esteban.roblesluna@gmail.com>
2013-10-28 14:07:48 -0300

    Add application id to the message

--- modules/actions-core/src/main/java/com/abstractions/utils/MessageUtils.java
+++ modules/actions-core/src/main/java/com/abstractions/utils/MessageUtils.java
@@ -3,7 +3,12 @@ package com.abstractions.utils;
 public class MessageUtils {
 
 	public static final String BASE_PROPERTY = "actions";
+	
 	public static final String HTTP_BASE_PROPERTY = BASE_PROPERTY + ".http";
 	public static final String FILE_BASE_PROPERTY = BASE_PROPERTY + ".file";
 
+	public static final String APPLICATION_BASE_PROPERTY = BASE_PROPERTY + ".application";
+
+	public static final String APPLICATION_ID_PROPERTY = APPLICATION_BASE_PROPERTY + ".id";
+	public static final String APPLICATION_PROPERTY_BASE_PROPERTY = APPLICATION_BASE_PROPERTY + ".property";
 }
