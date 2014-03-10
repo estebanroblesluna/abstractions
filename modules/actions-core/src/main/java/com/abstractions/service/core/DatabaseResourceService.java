ef414d3a38461bef8903201ada419bb8aa3b9b57
jspurim <jspurim@gmail.com>
2014-03-10 09:01:08 -0300

    Added files that were left out in the last commit.

---
+++ modules/actions-core/src/main/java/com/abstractions/service/core/DatabaseResourceService.java
@@ -0,0 +1,115 @@
+package com.abstractions.service.core;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jsoup.helper.Validate;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.abstractions.model.Resource;
+import com.abstractions.repository.GenericRepository;
+
+public class DatabaseResourceService implements ResourceService{
+
+	private static Log log = LogFactory.getLog(DatabaseResourceService.class);
+	private String resourceType;
+	private GenericRepository repository;
+	
+	private HashMap<String,Object> MakeSearchRestrictions(long applicationId, String path){
+		HashMap<String, Object> restrictions = new HashMap<String,Object>();
+		restrictions.put("applicationId", applicationId);
+		restrictions.put("path",path);
+		restrictions.put("type", this.resourceType);
+		return restrictions;
+	}
+	
+	public DatabaseResourceService(GenericRepository repository, String resourceType){
+		Validate.notNull(repository);
+		this.repository = repository;
+		this.resourceType = resourceType;
+	}
+	
+	@Override
+	@Transactional
+	public void storeResource(long applicationId, String path, InputStream stream){
+		Resource resource;
+		this.deleteResource(applicationId, path);
+		try {
+			resource = new Resource(applicationId,path,IOUtils.toByteArray(stream),this.resourceType);
+			repository.save(resource);
+		} catch (IOException e) {
+			log.error("Error storing resource", e);
+		}
+	}
+	
+	@Override
+	@Transactional(readOnly=true)
+	public InputStream getContentsOfResource(long applicationId, String path){
+		Resource resource = this.repository.findBy(Resource.class,
+				this.MakeSearchRestrictions(applicationId, path));
+		return new ByteArrayInputStream(resource.getData());
+	}
+	
+	@Override
+	@Transactional(readOnly=true)
+	public List<String> listResources(long applicationId){
+		List<Resource> resources = this.repository.findAllBy(Resource.class, "applicationId", applicationId);
+		List<String> ret = new ArrayList<String>();
+		for(Resource res : resources)
+			ret.add(res.getPath());
+		return ret;
+	}
+	
+	@Override
+	@Transactional
+	public void uncompressContent(long applicationId, InputStream stream){
+		try {
+			ZipInputStream zipInputStream = new ZipInputStream(stream);
+			ZipEntry zipEntry = zipInputStream.getNextEntry();
+			while (zipEntry != null) {
+				if (zipEntry.getSize() == -1) {
+					zipInputStream.closeEntry();
+					zipEntry = zipInputStream.getNextEntry();
+					continue;
+				}
+				this.storeResource(applicationId, zipEntry.getName().replace("." + File.separator,  File.separator), zipInputStream);
+				zipInputStream.closeEntry();
+				zipEntry = zipInputStream.getNextEntry();
+			}
+			zipInputStream.close();
+		} catch (Exception e) {
+			log.error("Error uncompressing content", e);
+		}
+	}
+	
+	@Override
+	@Transactional
+	public void deleteResource(long applicationId, String path)
+	{
+		Resource toDelete = this.repository.findBy(Resource.class,
+				this.MakeSearchRestrictions(applicationId, path));
+		if(toDelete != null)
+			this.repository.delete(Resource.class,toDelete.getId());
+	}
+	
+	public boolean resourceExists(long applicationId, String path){
+		return this.repository.findBy(Resource.class,
+				this.MakeSearchRestrictions(applicationId, path)) != null;
+	}
+	
+	public long getResourceLastModifiedDate(long applicationId, String path){
+		return this.repository.findBy(Resource.class,
+				this.MakeSearchRestrictions(applicationId, path)).getLastModifiedDate().getTime();
+	}
+	
+}
---
+++ modules/actions-editor/src/main/java/com/abstractions/service/rest/FileRESTService.java
@@ -0,0 +1,92 @@
+package com.abstractions.service.rest;
+
+import java.io.InputStream;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.abstractions.service.SnapshotService;
+import com.abstractions.service.core.ResourceService;
+import com.abstractions.utils.JsonBuilder;
+
+@Component
+@Path("/fileStore")
+public class FileRESTService {
+
+	private static Log log = LogFactory.getLog(FileRESTService.class);
+	
+	@Autowired
+	private ResourceService fileService;
+	@Autowired
+	private SnapshotService snapshotService;
+
+	public FileRESTService() {
+	}
+
+	@GET
+	@Path("{applicationId}/files/")
+	public Response getAllFiles(@PathParam("applicationId") long applicationId) {
+		try {
+			JsonBuilder builder = JsonBuilder.newWithArray("files");
+			for (String filename : this.fileService.listResources(applicationId)) {
+				builder.string(filename);
+			}
+			builder.endArray();
+			return Response.ok(builder.getContent()).build();
+		} catch (Exception e) {
+			log.error("Error building file list", e);
+		}
+		return Response.status(404).entity("Files not found").build();
+	}
+
+	@GET
+	@Path("{applicationId}/files/{filePath:.+}")
+	public Response getFile(@PathParam("applicationId") long applicationId, @PathParam("filePath") String path) {
+		InputStream result = this.fileService.getContentsOfResource(applicationId, path);
+		if (result == null) {
+			return Response.status(404).entity("File not found").build();
+		}
+		return Response.ok(result).build();
+	}
+	
+	@DELETE
+	@Path("{applicationId}/files/{filePath:.+}")
+	public Response deleteFile(@PathParam("applicationId") long applicationId, @PathParam("filePath") String path) {
+		this.fileService.deleteResource(applicationId, path);
+		return Response.ok("File deleted").build();
+	}
+
+	@PUT
+	@Path("{applicationId}/files/{filePath:.+}")
+	public Response putFile(@PathParam("applicationId") long applicationId, @PathParam("filePath") String path, @RequestBody InputStream stream) {
+		this.fileService.storeResource(applicationId, path, stream);
+		return Response.ok("File stored").build();
+	}
+
+	@PUT
+	@Path("{applicationId}/files/compressed")
+	public Response postCompressedFiles(@PathParam("applicationId") long applicationId, @RequestBody InputStream stream) {
+		this.fileService.uncompressContent(applicationId, stream);
+		return Response.ok("Files uncompressed").build();
+	}
+	
+	@GET
+	@Path("{applicationId}/snapshots/{snapshotId}")
+	public Response getSnapshot(@PathParam("applicationId") String applicationId, @PathParam("snapshotId") String snapshotId) {
+		InputStream result = this.snapshotService.getContentsOfSnapshot(applicationId, snapshotId);
+		if (result == null) {
+			return Response.status(404).entity("File not found").build();
+		}
+		return Response.ok(result).build();
+	}
+}
