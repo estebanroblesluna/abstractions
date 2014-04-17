e2b4de1223ac447b0b34739a2991af44af6339df
matias <jose.matias.rivero@gmail.com>
2014-04-17 12:59:32 -0300

    - Migrated initial resources from filesystem to DB - BLOB datatype extended in resource table

--- config/files/recreateFiles.sh
+++
@@ -1,5 +0,0 @@
-BASEDIR=$(dirname $0)
-
-rm -Rf $BASEDIR/../../modules/actions-editor/files;
-mkdir $BASEDIR/../../modules/actions-editor/files;
-cp -Rf $BASEDIR/2 $BASEDIR/../../modules/actions-editor/files;
