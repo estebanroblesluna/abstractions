0274254bcb06ceae905e29214af67edb4262e7b4
guidocelada <celadaguido@gmail.com>
2014-04-23 12:26:02 -0300

    Send email on account enabled

--- modules/actions-editor/src/main/java/com/abstractions/web/AdminPanelController.java
+++ modules/actions-editor/src/main/java/com/abstractions/web/AdminPanelController.java
@@ -1,5 +1,7 @@
 package com.abstractions.web;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,6 +20,12 @@ public class AdminPanelController {
 	@Autowired
 	CustomJdbcUserDetailsManager userManager; 
 	
+	@Autowired
+	EmailService emailService;
+	
+	private static Log log = LogFactory.getLog(AdminPanelController.class);
+
+	
 	@RequestMapping(value="/admin/enable-users", method = RequestMethod.GET)
 	public ModelAndView enableUsers() {
 		ModelAndView mv = new ModelAndView("admin-enable-users", "EnableUserForm", new EnableUserForm());
@@ -29,6 +37,11 @@ public class AdminPanelController {
 	public String receiveEnableUsers(@ModelAttribute("form") EnableUserForm form) {
 		for (String username : form.getUsersToEnable()) {
 			userManager.enableUser(username);
+			try {
+				emailService.sendUserEnabledMail(userManager.loadUserByUsernameWithoutAuths(username));
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
 		}
 		return "redirect:/admin/enable-users";
 	}
