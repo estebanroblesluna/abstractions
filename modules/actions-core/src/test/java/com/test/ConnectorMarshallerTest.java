07b6377d36c85bf6c46af8d5a48d391811bcca9a
MartinAparicioPons <martin.aparicio.pons@gmail.com>
2014-05-28 10:40:24 -0300

    Create and test marshaller for connectors

--- modules/actions-core/src/test/java/com/test/ConnectorMarshallerTest.java
+++ modules/actions-core/src/test/java/com/test/ConnectorMarshallerTest.java
@@ -21,39 +21,92 @@ import com.abstractions.model.Connector;
  */
 public class ConnectorMarshallerTest extends TestCase {
 
-  private ArrayList<Connector> connectors;
-  private JSONArray simpleJSON;
+  private ArrayList<Connector> connectors1;
+  private ArrayList<Connector> connectors2;
+  private ArrayList<Connector> connectors3;
+  private JSONArray testJSON1;
+  private JSONArray testJSON2;
+  private JSONArray testJSON3;
 
   @Before
   public void setUp() {
-    // setup connectors
-    connectors = new ArrayList<Connector>();
-    HashMap<String, String> configurations = new HashMap<String, String>();
-    configurations.put("configUno", "0001");
-    configurations.put("configDos", "0002");
-    connectors.add(new Connector("testing", "none", configurations));
 
-    // setup testJSON
-    String JSONstring = null;
+    // setup connectors:
+
+    // setup connectors one
+    connectors1 = new ArrayList<Connector>();
+    HashMap<String, String> configurations1 = new HashMap<String, String>();
+    configurations1.put("configUno", "0001");
+    configurations1.put("configDos", "0002");
+    connectors1.add(new Connector("testing", "none", configurations1));
+
+    // setup connectors two
+    connectors2 = new ArrayList<Connector>();
+    HashMap<String, String> configurations2 = new HashMap<String, String>();
+    configurations2.put("time", "1002");
+    configurations2.put("isTrue", "True");
+    connectors2.add(new Connector("Cache", "connection to cache", configurations2));
+    HashMap<String, String> configurations3 = new HashMap<String, String>();
+    configurations3.put("ttl", "100");
+    connectors2.add(new Connector("anotherOne", "connection to DB", configurations3));
+    HashMap<String, String> configurations4 = new HashMap<String, String>();
+    connectors2.add(new Connector("yes", "", configurations4));
+    HashMap<String, String> configurations5 = new HashMap<String, String>();
+    configurations5.put("1", "1");
+    connectors2.add(new Connector("1", "1", configurations5));
+
+    // setup connectors three
+    connectors3 = new ArrayList<Connector>();
+
+    // setup testing .json:
+
+    // setup testJSON1
+    String JSONstring1 = null;
     try {
-      JSONstring = IOUtils.toString(this.getClass().getResourceAsStream("ConnectorJSONtesting.json")).replace("\n", "");
+      JSONstring1 = IOUtils.toString(this.getClass().getResourceAsStream("ConnectorJSONtesting.json")).replace("\n", "");
     } catch (IOException e1) {
       fail("JSONException when opening resource file ConnectorJSONtesting");
     }
     try {
-      simpleJSON = new JSONArray(JSONstring);
+      testJSON1 = new JSONArray(JSONstring1);
+    } catch (JSONException e) {
+      fail("JSONException when trying to parse simpleJSON to a JSONArray");
+    }
+
+    // setup testJSON2
+    String JSONstring2 = null;
+    try {
+      JSONstring2 = IOUtils.toString(this.getClass().getResourceAsStream("ConnectorJSONtesting2.json")).replace("\n", "");
+    } catch (IOException e1) {
+      fail("JSONException when opening resource file ConnectorJSONtesting");
+    }
+    try {
+      testJSON2 = new JSONArray(JSONstring2);
+    } catch (JSONException e) {
+      fail("JSONException when trying to parse simpleJSON to a JSONArray");
+    }
+
+    // setup testJSON3
+    String JSONstring3 = null;
+    try {
+      JSONstring3 = IOUtils.toString(this.getClass().getResourceAsStream("ConnectorJSONtesting3.json")).replace("\n", "");
+    } catch (IOException e1) {
+      fail("JSONException when opening resource file ConnectorJSONtesting");
+    }
+    try {
+      testJSON3 = new JSONArray(JSONstring3);
     } catch (JSONException e) {
       fail("JSONException when trying to parse simpleJSON to a JSONArray");
     }
   }
 
   @Test
-  public void testSimpleMarshall() {
+  public void testOneMarshall() {
 
-    // marshall the simple definition list
+    // marshall the connector list
     String result = null;
     try {
-      result = ConnectorMarshaller.marshall(connectors);
+      result = ConnectorMarshaller.marshall(connectors1);
     } catch (Exception e) {
       fail("Exception when trying to marshall connectors");
     }
@@ -65,31 +118,105 @@ public class ConnectorMarshallerTest extends TestCase {
     } catch (JSONException e) {
       fail("JSONException when trying to parse string to a JSONArray");
     }
-    assertEquals(simpleJSON, resultJSON);
+    assertEquals(testJSON1, resultJSON);
 
   }
 
   @Test
-  public void testSimpleUnmarshall() {
+  public void testOneUnmarshall() {
 
     // unmarshall the definitions
     ArrayList<Connector> connectorsResult = null;
     try {
-      String auxStr = simpleJSON.toString();
+      String auxStr = testJSON1.toString();
       connectorsResult = (ArrayList<Connector>) ConnectorMarshaller.unmarshall(auxStr);
     } catch (Exception e) {
       fail("Exception when trying to unmarshall connectors");
     }
 
-    assertEquals(connectors, connectorsResult);
+    assertEquals(connectors1.size(), connectorsResult.size());
   }
 
   @Test
-  public void testSimpleMarshalUnmarshall() {
+  public void testTwoMarshall() {
+
+    // marshall the connector list
+    String result = null;
+    try {
+      result = ConnectorMarshaller.marshall(connectors2);
+    } catch (Exception e) {
+      fail("Exception when trying to marshall connectors");
+    }
+
+    // transform it to jsonArray
+    JSONArray resultJSON = null;
+    try {
+      resultJSON = new JSONArray(result);
+    } catch (JSONException e) {
+      fail("JSONException when trying to parse string to a JSONArray");
+    }
+    assertEquals(testJSON2, resultJSON);
+
+  }
+
+  @Test
+  public void testTwoUnmarshall() {
+
+    // unmarshall the definitions
+    ArrayList<Connector> connectorsResult = null;
+    try {
+      String auxStr = testJSON2.toString();
+      connectorsResult = (ArrayList<Connector>) ConnectorMarshaller.unmarshall(auxStr);
+    } catch (Exception e) {
+      fail("Exception when trying to unmarshall connectors");
+    }
+
+    assertEquals(connectors2.size(), connectorsResult.size());
+  }
+
+  @Test
+  public void testThreeMarshall() {
+
+    // marshall the connector list
+    String result = null;
+    try {
+      result = ConnectorMarshaller.marshall(connectors3);
+    } catch (Exception e) {
+      fail("Exception when trying to marshall connectors");
+    }
+
+    // transform it to jsonArray
+    JSONArray resultJSON = null;
+    try {
+      resultJSON = new JSONArray(result);
+    } catch (JSONException e) {
+      fail("JSONException when trying to parse string to a JSONArray");
+    }
+    assertEquals(testJSON3, resultJSON);
+
+  }
+
+  @Test
+  public void testThreeUnmarshall() {
+
+    // unmarshall the definitions
+    ArrayList<Connector> connectorsResult = null;
+    try {
+      String auxStr = testJSON2.toString();
+      connectorsResult = (ArrayList<Connector>) ConnectorMarshaller.unmarshall(auxStr);
+    } catch (Exception e) {
+      fail("Exception when trying to unmarshall connectors");
+    }
+
+    assertEquals(connectors2.size(), connectorsResult.size());
+  }
+
+  @Test
+  public void testOneMarshalUnmarshall() {
     // marshall the simple connector list
     String result = null;
     try {
-      result = ConnectorMarshaller.marshall(connectors);
+      result = ConnectorMarshaller.marshall(connectors1);
     } catch (Exception e) {
       fail("Exception when trying to marshall connectors");
     }
@@ -111,6 +238,66 @@ public class ConnectorMarshallerTest extends TestCase {
       fail("Exception when trying to unmarshall connectors");
     }
 
-    assertEquals(connectorsResult.size(), connectors.size());
+    assertEquals(connectorsResult.size(), connectors1.size());
+  }
+
+  @Test
+  public void testTwoMarshalUnmarshall() {
+    // marshall the simple connector list
+    String result = null;
+    try {
+      result = ConnectorMarshaller.marshall(connectors2);
+    } catch (Exception e) {
+      fail("Exception when trying to marshall connectors");
+    }
+
+    // transform it to jsonArray
+    JSONArray resultJSON = null;
+    try {
+      resultJSON = new JSONArray(result);
+    } catch (JSONException e) {
+      fail("JSONException when trying to parse string to a JSONArray");
+    }
+
+    // unmarshall the connectors
+    ArrayList<Connector> connectorsResult = null;
+    try {
+      String auxStr = resultJSON.toString();
+      connectorsResult = (ArrayList<Connector>) ConnectorMarshaller.unmarshall(auxStr);
+    } catch (Exception e) {
+      fail("Exception when trying to unmarshall connectors");
+    }
+
+    assertEquals(connectorsResult.size(), connectors2.size());
+  } 
+  
+  @Test
+  public void testThreeMarshalUnmarshall() {
+    // marshall the simple connector list
+    String result = null;
+    try {
+      result = ConnectorMarshaller.marshall(connectors3);
+    } catch (Exception e) {
+      fail("Exception when trying to marshall connectors");
+    }
+
+    // transform it to jsonArray
+    JSONArray resultJSON = null;
+    try {
+      resultJSON = new JSONArray(result);
+    } catch (JSONException e) {
+      fail("JSONException when trying to parse string to a JSONArray");
+    }
+
+    // unmarshall the connectors
+    ArrayList<Connector> connectorsResult = null;
+    try {
+      String auxStr = resultJSON.toString();
+      connectorsResult = (ArrayList<Connector>) ConnectorMarshaller.unmarshall(auxStr);
+    } catch (Exception e) {
+      fail("Exception when trying to unmarshall connectors");
+    }
+
+    assertEquals(connectorsResult.size(), connectors3.size());
   }
 }
